0f470b88514145a41a6c4621fb8cd15597c556c4
saptarshi sanyla <saptarshi.edp@gmail.com>
2015-09-08 14:50:45 +0530

    commit

--- billdeskws/src/in/net/dpl/rest/BilldeskResponse.java
+++ billdeskws/src/in/net/dpl/rest/BilldeskResponse.java
@@ -15,29 +15,7 @@ import javax.ws.rs.core.Response;
  
 @Path("/direct")
 public class BilldeskResponse {
-	@GET
-	@Produces("application/xml")
-	public String convertCtoF() {
- 
-		Double fahrenheit;
-		Double celsius = 36.8;
-		fahrenheit = ((celsius * 9) / 5) + 32;
- 
-		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
-		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
-	}
- 
-	@Path("{c}")
-	@GET
-	@Produces("application/xml")
-	public String convertCtoFfromInput(@PathParam("c") Double c) {
-		Double fahrenheit;
-		Double celsius = c;
-		fahrenheit = ((celsius * 9) / 5) + 32;
- 
-		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
-		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
-	}
+	
 	@POST
 	@Path("/response")
 	public String addUser(
