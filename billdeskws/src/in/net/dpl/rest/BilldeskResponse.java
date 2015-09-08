526b7cbad8744fd43fe71f9f7e23e050649ba234
saptarshi sanyla <saptarshi.edp@gmail.com>
2015-09-08 13:54:33 +0530

    Commit at 2.00 PM on 08/09/2015

---
+++ billdeskws/src/in/net/dpl/rest/BilldeskResponse.java
@@ -0,0 +1,52 @@
+package in.net.dpl.rest;
+ 
+import javax.ws.rs.FormParam;
+
+/**
+ * @author Crunchify.com
+ */
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+ 
+@Path("/direct")
+public class BilldeskResponse {
+	@GET
+	@Produces("application/xml")
+	public String convertCtoF() {
+ 
+		Double fahrenheit;
+		Double celsius = 36.8;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+ 
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+ 
+	@Path("{c}")
+	@GET
+	@Produces("application/xml")
+	public String convertCtoFfromInput(@PathParam("c") Double c) {
+		Double fahrenheit;
+		Double celsius = c;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+ 
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+	@POST
+	@Path("/response")
+	public String addUser(
+		@FormParam("msg") String msg) {
+		System.out.println("MSG-"+msg);
+		//return Response.status(200).entity("MSG is called, name : " + msg).build();
+		return "200";
+		
+
+	}
+
+}
